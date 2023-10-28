689df2b174c92117b18890651b4a3a65b61b24cb
Tyler Shepherd <100872070+TylerShep@users.noreply.github.com>
2023-10-28 17:10:07 -0600

    Update file.js

--- app/static/file.js
+++ app/static/file.js
@@ -1,1 +1,5 @@
-
+function submitRequest() {
+    var request = document.getElementById('search-box').value;
+    console.log("Request submitted:", request);
+    // Here you can add code to push the request to another source
+}
